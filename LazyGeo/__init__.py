c033737d1b92933adf6673e39b126fb9474037ee
Rafael Muzafarov <catspriteee@gmail.com>
2026-06-12 23:31:45 +0300

    Delete LazyGeo directory

--- LazyGeo/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "libs"))
-
-def classFactory(iface):
-    from .lazygeo import LazyGeoPlugin
-    return LazyGeoPlugin(iface)
